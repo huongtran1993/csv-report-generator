d641bf527bd734b07a5e2bfd1a8aa3311ebac461
huongtran1993 <huongtran1993@gmail.com>
2021-08-31 17:50:33 -0700

    refactor to use a file picker instead of a textarea

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const multer = require('multer');
+var uploadedfilePath = path.join(__dirname, '/../uploads/');
 const upload = multer();
 const port = 3000;
 const Utils = require('./lib/utils')
@@ -12,8 +13,8 @@ app.use(express.json());
 app.use(express.static(__dirname + '/../client'));
 app.listen(port);
 
-app.post('/upload_json', upload.none(), function(req, res) {
-  var dataStr = Utils.flatten(JSON.parse(req.body['data-input']));
+app.post('/upload_json', upload.single('data-input'), function(req, res) {
+  var dataStr = Utils.flatten(JSON.parse(req.file.buffer));
   var filePath = path.join(__dirname, '/../data/csv_report.csv');
   console.log(filePath);
   Utils.create(filePath, dataStr, function(err, result) {
